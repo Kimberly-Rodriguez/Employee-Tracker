2e44de949ef230d057ad5d09903907af6bed2eca
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2021-09-16 20:09:02 -0700

    input of seeds + queries

--- db/query.sql
+++ db/query.sql
@@ -1,16 +1,40 @@
--- -- Query to Render All Movies --
--- SELECT * FROM movies;
 
--- -- Query to Insert Movie --
--- INSERT INTO movies (title) VALUES (title);
--- INSERT INTO reviews (review) VALUES (review);
 
--- -- Query to Update a Movie --
--- UPDATE reviews SET review = ? WHERE id = ?;
+SELECT name, id FROM department
 
--- -- Query to Delete a Movie --
--- DELETE FROM movies WHERE id = ?;
--- DELETE FROM reviews WHERE id = ?;
+-- aliasing
+SELECT r.title, r.id AS role_id, d.id AS department_id, d.name, r.salary
+FROM role r 
+JOIN department  d
+on r.department_id = d.id;
 
--- -- Query to Render Movies w/ their Reviews --
--- SELECT movies.title AS Title, reviews.review AS Reviews FROM reviews JOIN movies ON reviews.movies_id = movies.id;
+
+SELECT e.id, e.first_name, e.last_name, r.title, d.name, r.salary, e.manager_id
+FROM employee e
+JOIN role r 
+ON r.id = e.role_id 
+JOIN department d
+ON  d.id = r.department_id
+
+INSERT INTO department (name) VALUES ?
+
+
+-- OLD CODE
+-- Query to Render all employee --
+SELECT * FROM role;
+SELECT * FROM employee;
+
+
+-- Query to Insert --
+INSERT INTO role (title, salary, department_id) VALUES (title, salary, department_id);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (first_name, last_name, role_id, manager_id);
+
+-- Query to Update  --
+UPDATE role SET employee = ? WHERE id = ?;
+
+-- Query to Delete a  --
+DELETE FROM role WHERE id = ?;
+DELETE FROM employee WHERE id = ?;
+
+-- Query to Render Movies w/ their Reviews --
+SELECT role.title AS Title, employee.employee AS Role FROM employee JOIN role ON role.employee_id = role.id;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,20 @@
+
+INSERT INTO department (name)
+VALUES ("Engineering"),
+       ("Law"),
+       ("Accounting");
+
 INSERT INTO role (title, salary, department_id)
 VALUES ("Software Engineering", 130000, 01),
-       ("Account Manager", 160000, 02 ),
-       ("Lead Engineer", 150000, 03),
-       ("Accountant" 125000, 04),
-       ("Legal Lead", 250000, 05);
+       ("Account Manager", 160000, 03 ),
+       ("Lead Engineer", 150000, 01),
+       ("Accountant", 125000, 03),
+       ("Legal Lead", 250000, 02);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Sonia", "Sotomayor", 01, 1),
-("Maria", "Hinojosa", 02, 2), 
-("Selena", "Quintanilla", 02, 3),
-("Frida", "Kahlo", 03, 4),
-("Celia", "Cruz", 04, 5); 
+VALUES ("Sonia", "Sotomayor", 01, NULL),
+       ("Maria", "Hinojosa", 02, 01), 
+       ("Selena", "Quintanilla", 02, NULL),
+       ("Frida", "Kahlo", 03, 02),
+       ("Celia", "Cruz", 04, NULL); 
  
