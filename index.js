5a3f5f45254f48bd1a42127ef13c735a046a537c
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2021-09-17 16:02:46 -0700

    added switch statement

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 require('console.table');
-// Import and require mysql2
 const mysql = require('mysql2');
 
 // Connect to database
@@ -38,28 +37,37 @@ const init = () => {
       },
     ])
     .then((response) => {
-      //   const managerData = new Manager (response.name, response.id, response.email, response.officeNumber)
-      // //push to my teamData array 
-      //   teamData.push(managerData);
-        
-        return (response.choice === 'View All Employees') ? ViewAllEmployees()
-        : (response.choice === 'Add Employee') ? AddEmployee()
-        : (response.choice === 'Update Employee Role') ? UpdateEmployeeRole()
-        : (response.choice === 'View All Roles') ? ViewAllRoles()
-        : (response.choice === 'Add Role') ? AddRole()
-        : (response.choice === 'View All Departments') ? ViewAllDepartments()
-        : (response.choice === 'Add Department') ? AddDepartment()
-      
-        //Edit format below:
-        : fs.writeFile('./dist/', generateHtml(teamData), (err) =>
-          err ? console.log(err) : console.log('Success!'))
-          });
-}
+      switch (response.choice) {
+        case 'View All Employees':
+        ViewAllEmployees()
+      break;
+        case 'Add Employee':
+         AddEmployee()
+       break;
+       case 'Update Employee Role':
+         UpdateEmployeeRole()
+       break;
+       case 'View All Roles':
+         ViewAllRoles()
+       break;
+       case 'Add Role':
+         AddRole()
+       break;
+       case 'View All Departments':
+         ViewAllDepartments()
+       break;
+       case 'Add Department':
+         AddDepartment()
+       break;
+     }
+    });
 
+  }
+  
 
 // View All Employees function
 const ViewAllEmployees = () => {
-// Read 
+
 const sql = `SELECT * FROM employee`;
   db.query(sql, (err, rows) => {
     if (err) {
@@ -71,11 +79,17 @@ const sql = `SELECT * FROM employee`;
 });
 }
 
+// asking what is the employ name 
+
+
+
+// then call add employe the answers from this funtion
+
 // Add Employee function
-const AddEmployee = () => {
-  const sql = `INSERT INTO movies (e)
+const AddEmployee = (answer) => {
+  const sql = `INSERT INTO employee e (e.first_name, e.last_name, e.role_id, e.manager_id)
     VALUES (?)`;
-  const params = [body.movie_name];
+  const params = [answer.first_name, ]// whatever answr for athe add employee]];
   // 31 - 40 is more or less the same just the params might change
   db.query(sql, params, (err, result) => {
     if (err) {
@@ -92,85 +106,45 @@ const AddEmployee = () => {
 
   // Up date Employee Role Function
   const UpdateEmployeeRole = () => {
-    inquirer
-    .prompt([
-  {
-      // UpdateEmployeeRole
-  },
-])
- // .then((response) => {
-    //   const internData = new Intern (response.name, response.id, response.email, response.school)
-    // //push to my teamData array 
-    //   teamData.push(internData);
-      
-    //   return (response.role === 'Engineer') ? createNewEngineer()
-    //   : (response.role === 'Intern') ? createNewIntern()
-    //   : fs.writeFile('./dist/index.html', generateHtml(teamData), (err) =>
-    //     err ? console.log(err) : console.log('Success!'))
-    //   });
+   
   }
 
 // Update View All Roles Function  
 const ViewAllRoles = () => {
-  inquirer
-.prompt([
-  {
-      // table of all Roles
-  },
-])
+
+  const sql = `SELECT * FROM role`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+       return;
+    }
+    console.table(rows);
+    init();
+});
+
 }
 
 // Add Role Function
 const AddRole = () => {
-  inquirer
-  .prompt([
-{
-    // Add New Role 
-},
-])
-// .then((response) => {
-  //   const internData = new Intern (response.name, response.id, response.email, response.school)
-  // //push to my teamData array 
-  //   teamData.push(internData);
-    
-  //   return (response.role === 'Engineer') ? createNewEngineer()
-  //   : (response.role === 'Intern') ? createNewIntern()
-  //   : fs.writeFile('./dist/index.html', generateHtml(teamData), (err) =>
-  //     err ? console.log(err) : console.log('Success!'))
-  //   });
+
 }
 
 // View All Departments Function
 const ViewAllDepartments = () => {
-  inquirer
-.prompt([
-  {
-      // table of all Departments
-  },
-])
+  const sql = `SELECT * FROM department`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+       return;
+    }
+    console.table(rows);
+    init();
+});
 }
 
 // Add Department Function
 const AddDepartment = () => {
-  inquirer
-  .prompt([
-{
-    // Add New Role 
-},
-])
-// .then((response) => {
-  //   const internData = new Intern (response.name, response.id, response.email, response.school)
-  // //push to my teamData array 
-  //   teamData.push(internData);
-    
-  //   return (response.role === 'Engineer') ? createNewEngineer()
-  //   : (response.role === 'Intern') ? createNewIntern()
-  //   : fs.writeFile('./dist/index.html', generateHtml(teamData), (err) =>
-  //     err ? console.log(err) : console.log('Success!'))
-  //   });
 }
 
 // Function call to initialize app
 init();
-
-module.exports = init;
